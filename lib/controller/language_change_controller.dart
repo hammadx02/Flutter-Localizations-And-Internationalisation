06c1038ebe32c03e2e3bbebfdfd4656cd691b757
Hammad Ali <hammadaali02@gmail.com>
2024-08-10 15:09:19 +0500

    updated

--- lib/controller/language_change_controller.dart
+++ lib/controller/language_change_controller.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LanguageChangeController with ChangeNotifier {
-  
+  Locale? _appLocale;
+  Locale? get appLocale => _appLocale;
+
+  void changeLanguage(Locale type) async {
+    SharedPreferences sp = await SharedPreferences.getInstance();
+
+    if (type == Locale('en')) {
+      await sp.setString('language_code', 'en');
+    } else {
+      await sp.setString('language_code', 'es');
+    }
+  }
+
+  notifyListeners();
 }
