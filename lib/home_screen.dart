4b4f6b5bb97af3b5959e28cd75ffeb59b117248f
Hammad Ali <hammadaali02@gmail.com>
2024-08-10 15:25:45 +0500

    updated

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations_and_internationalisation/controller/language_change_controller.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -7,6 +9,8 @@ class HomeScreen extends StatefulWidget {
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
+
+enum Languange { english, spanish }
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
@@ -16,6 +20,25 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text(
           AppLocalizations.of(context)!.helloWorld,
         ),
+        actions: [
+          Consumer<LanguageChangeController>(
+            builder: (context, provider, child) {
+              return PopupMenuButton(
+                itemBuilder: (BuildContext context) =>
+                    <PopupMenuEntry<Languange>>[
+                  const PopupMenuItem(
+                    value: Languange.english,
+                    child: Text('English'),
+                  ),
+                  const PopupMenuItem(
+                    value: Languange.spanish,
+                    child: Text('Spanish'),
+                  ),
+                ],
+              );
+            },
+          ),
+        ],
       ),
     );
   }
